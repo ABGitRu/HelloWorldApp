e8a623a4e73d65c9fd6cfede58f38146e60dbbc9
Alexey Barinov <begoodbebad2012@gmail.com>
2021-03-22 23:12:10 +0300

    button implementation

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var showTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textLabel.isHidden = true
+        showTextButton.layer.cornerRadius = 10
     }
+    
+    
 
+    @IBAction func showTextButtonTapped() {
+        textLabel.isHidden.toggle()
+        
+        if textLabel.isHidden {
+            showTextButton.setTitle("Show text", for: .normal)
+        } else {
+            showTextButton.setTitle("Hide text", for: .normal)
+        }
+    }
+    
 
 }
 
